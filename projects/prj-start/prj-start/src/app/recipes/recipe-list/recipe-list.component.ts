8b24730f352b66255a24dfe4fa990023f38653df
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-15 16:34:21 +0530

    48: added single recipe item; next: run through all items in the recipes list

--- projects/prj-start/prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
+++ projects/prj-start/prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This. is a simple test', 'https://www.tasteofhome.com/wp-content/uploads/2018/01/Garlic-Lemon-Shrimp_EXPS_SDON16_45286__D06_09_5b-696x696.jpg')
+  ];
 
   constructor() { }
 
