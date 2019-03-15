69290298a4df64e86dd41fae4762e98b55e6bd3f
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-15 18:13:07 +0530

    49: show recipe list

--- projects/prj-start/prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
+++ projects/prj-start/prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,10 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'This. is a simple test', 'https://www.tasteofhome.com/wp-content/uploads/2018/01/Garlic-Lemon-Shrimp_EXPS_SDON16_45286__D06_09_5b-696x696.jpg')
+    new Recipe('A test Recipe', 'This. is a simple test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Pizza_%2822%29.jpg/120px-Pizza_%2822%29.jpg'),
+    new Recipe('A test Recipe', 'This. is a simple test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Pizza_%2822%29.jpg/120px-Pizza_%2822%29.jpg'),
+    new Recipe('A test Recipe', 'This. is a simple test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Pizza_%2822%29.jpg/120px-Pizza_%2822%29.jpg'),
+    new Recipe('A test Recipe', 'This. is a simple test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Pizza_%2822%29.jpg/120px-Pizza_%2822%29.jpg')
   ];
 
   constructor() { }
