2b48c5bcec981b5d8a31acbc0146e4e660f95cfa
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-16 12:35:00 +0530

    property and event bindinng: 61

--- projects/cmp-databinding-start/cmp-databinding-start/src/app/app.component.ts
+++ projects/cmp-databinding-start/cmp-databinding-start/src/app/app.component.ts
@@ -1,28 +1,11 @@
 import { Component } from '@angular/core';
-
+import { CockpitComponent } from "./cockpit/cockpit.component";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
+  serverElements = [{type:'server', name:'testserver', content:'just a test!'}];
 
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
-
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
 }
