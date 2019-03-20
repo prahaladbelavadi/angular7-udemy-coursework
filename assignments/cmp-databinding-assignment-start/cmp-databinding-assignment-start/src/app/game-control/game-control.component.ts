ce22e24926535535bb4656b4c6a838e1064a0a5f
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-20 13:38:18 +0530

    finish assignment 4

--- assignments/cmp-databinding-assignment-start/cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
+++ assignments/cmp-databinding-assignment-start/cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+@Output() intervalFired = new EventEmitter<number>();
+interval;
+lastNumber = 0
 
-  constructor() { }
+constructor() { }
 
   ngOnInit() {
   }
+  onStartGame() {
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.lastNumber + 1);
+      this.lastNumber++
+    }, 1000)
+  }
 
+  onPauseGame(){
+    clearInterval(this.interval)
+  }
 }
