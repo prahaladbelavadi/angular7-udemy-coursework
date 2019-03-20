e56a3d5a25ce9abace98257dfb0a6c23bc520f72
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-20 12:52:48 +0530

    genrate gamecontrol, oddand even components

--- assignments/cmp-databinding-assignment-start/cmp-databinding-assignment-start/src/app/app.module.ts
+++ assignments/cmp-databinding-assignment-start/cmp-databinding-assignment-start/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
+import { GameControlComponent } from './game-control/game-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OddComponent,
+    EvenComponent,
+    GameControlComponent
   ],
   imports: [
     BrowserModule,
