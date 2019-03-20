da8d99579808f687cc64cfe47110738562552b55
Prahalad Belavadi <prahalad@belavadi.com>
2019-03-20 14:13:10 +0530

    Revert "Breaking at 64  binding to custom events and at output (#1)"
    This reverts commit 3e81abc61f50b040af84e2fb4c2068383475e719.

--- projects/cmp-databinding-start/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ projects/cmp-databinding-start/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,8 +6,6 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
-  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
   newServerName = '';
   newServerContent = '';
@@ -16,32 +14,19 @@ export class CockpitComponent implements OnInit {
 
   ngOnInit() {
   }
-
-  // onAddServer(){
-  //   // this.serverElements.push({
-  //   //   type:'server',
-  //   //   name: this.newServerName,
-  //   //   content: this.newServerContent
-  //   // })
-  // }
-
-  // onAddBlueprint() {
-  //   // this.serverElements.push({
-  //   //   type: 'blueprint',
-  //   //   name: this.newServerName,
-  //   //   content: this.newServerContent
-  //   // })
-  // }
-
   onAddServer() {
-    this.serverCreated.emit({
-      serverName: this.newServerName, serverContent: this.newServerContent
-    })
+    // this.serverElements.push({
+    //   type: 'server',
+    //   name: this.newServerName,
+    //   content: this.newServerContent
+    // });
   }
 
   onAddBlueprint() {
-    this.blueprintCreated.emit({
-      serverName: this.newServerName, serverContent: this.newServerContent
-    })
+    // this.serverElements.push({
+    //   type: 'blueprint',
+    //   name: this.newServerName,
+    //   content: this.newServerContent
+    // });
   }
 }
