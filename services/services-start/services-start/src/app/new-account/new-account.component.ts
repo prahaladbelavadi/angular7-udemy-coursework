24fb6103daee37a5fa37c5ca9aa29fa2036c2463
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-22 14:21:24 +0530

    99: export functionality(logging) to serviec

--- services/services-start/services-start/src/app/new-account/new-account.component.ts
+++ services/services-start/services-start/src/app/new-account/new-account.component.ts
@@ -1,12 +1,16 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from "../logging.service";
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private loggingService:LoggingService){}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
@@ -14,5 +18,7 @@ export class NewAccountComponent {
       status: accountStatus
     });
     console.log('A server status changed, new status: ' + accountStatus);
+
+    this.loggingService.logStatusChange(accountStatus)
   }
 }
