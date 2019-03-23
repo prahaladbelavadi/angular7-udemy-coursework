6fa2d863a313b16128afe72b0e9f7fad0671ddd3
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-23 18:29:18 +0530

    100: not working

--- services/services-start/services-start/src/app/new-account/new-account.component.ts
+++ services/services-start/services-start/src/app/new-account/new-account.component.ts
@@ -1,24 +1,20 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { LoggingService } from "../logging.service";
+import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  providers: [LoggingService, AccountsService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  constructor(private loggingService:LoggingService){}
+  constructor(private loggingService:LoggingService,
+    private accountService:AccountsService){}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
-
-    this.loggingService.logStatusChange(accountStatus)
-  }
+    this.accountService.addAccount(accountName, accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
+  };
 }
