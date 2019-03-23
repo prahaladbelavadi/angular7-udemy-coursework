cdf94adde3faf10ba2849621880ea6449a693a9a
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-23 19:06:51 +0530

    103: working- injecting services into services; note: @injectable goes on the receiving end of the service()

--- services/services-start/services-start/src/app/new-account/new-account.component.ts
+++ services/services-start/services-start/src/app/new-account/new-account.component.ts
@@ -6,7 +6,7 @@ import { AccountsService } from '../accounts.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
 
@@ -15,6 +15,6 @@ export class NewAccountComponent {
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
   };
 }
