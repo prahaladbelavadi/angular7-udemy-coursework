3cc7e8de3b8414a0f7f2548361dbe4c6eab6f9e5
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-25 17:49:02 +0530

    subscribe to the changes that happen since slice is operating on a differnt array

--- projects/prj-directives-final/prj-directives-final/src/app/shopping-list/shopping-list.component.ts
+++ projects/prj-directives-final/prj-directives-final/src/app/shopping-list/shopping-list.component.ts
@@ -15,5 +15,9 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
+    this.slService.ingredientsChanged
+    .subscribe((ingredients:Ingredient[])=>{
+      this.ingredients = ingredients;
+    })
   }
 }
--- projects/prj-directives-final/prj-directives-final/src/app/shopping-list/shopping-list.service.ts
+++ projects/prj-directives-final/prj-directives-final/src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,8 @@
 import { Ingredient } from "../shared/ingredient.model";
+import { EventEmitter } from "@angular/core";
 
 export class ShoppingListService{
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10),
@@ -12,6 +14,7 @@ export class ShoppingListService{
 
     addIngredient(ingredient:Ingredient){
         this.ingredients.push(ingredient)
+        this.ingredientsChanged.emit(this.ingredients.slice());
     }
 
 }
