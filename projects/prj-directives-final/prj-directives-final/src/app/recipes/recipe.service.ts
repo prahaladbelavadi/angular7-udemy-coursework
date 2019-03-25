6ae8d889e95341f1762c84f9233095f25a6c96cc
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-25 17:58:56 +0530

    112: add ingredients and make them showww

--- projects/prj-directives-final/prj-directives-final/src/app/recipes/recipe.service.ts
+++ projects/prj-directives-final/prj-directives-final/src/app/recipes/recipe.service.ts
@@ -1,16 +1,31 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
-export class RecipeService{
+export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe(
+            'A Test Recipe',
+            'This is simply a test',
+             'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 32)
+            ]),
+        new Recipe(
+            'Another Test Recipe',
+            'This is simply a test',
+            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', 
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 32)
+            ])
     ];
 
-    getRecipes(){
+    getRecipes() {
         return this.recipes.slice();
     }
 }
