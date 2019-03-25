f76dbda381d3e71974ac6ec5e7b78fd891490b09
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-25 13:28:41 +0530

    assignment: add user service

--- assignments/services-assignment-start/services-assignment-start/src/app/inactive-users/inactive-users.component.ts
+++ assignments/services-assignment-start/services-assignment-start/src/app/inactive-users/inactive-users.component.ts
@@ -1,15 +1,19 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-inactive-users',
   templateUrl: './inactive-users.component.html',
   styleUrls: ['./inactive-users.component.css']
 })
-export class InactiveUsersComponent {
-  @Input() users: string[];
-  @Output() userSetToActive = new EventEmitter<number>();
+export class InactiveUsersComponent implements OnInit{
+  users: string[];
 
+  constructor(private userService: UserService){  }
+  ngOnInit(){
+    this.users = this.userService.inactiveUsers;
+  }
   onSetToActive(id: number) {
-    this.userSetToActive.emit(id);
+    this.userService.setToActive(id)
   }
 }
