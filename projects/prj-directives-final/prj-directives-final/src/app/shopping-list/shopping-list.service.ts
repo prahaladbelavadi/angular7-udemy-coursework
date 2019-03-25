2f167475c2cfe37421ee3697b582fa35f040cde6
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-25 16:57:31 +0530

    110 attemmpt: not workinng

--- projects/prj-directives-final/prj-directives-final/src/app/shopping-list/shopping-list.service.ts
+++ projects/prj-directives-final/prj-directives-final/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,14 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class ShoppingListService{
-    
+    ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+    ];
+    addIngredients(ingredient:Ingredient){
+        this.ingredients.push(ingredient);
+    }
+    getIngredients(){
+        return this.ingredients.slice()
+    }
 }
