3e9250ec6ff95356f41e9eeb63342d72aa983796
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-26 18:23:34 +0530

    accessing edit server based on access control

--- routing/routing-start/routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing/routing-start/routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,27 +12,18 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(private serversService: ServersService, private route:ActivatedRoute) { }
 
   ngOnInit() {
-    // snapshot
-          console.log(this.route.snapshot.queryParams);
-          console.log(this.route.snapshot.fragment)
-    
-    //Observable method dynamic 
-          this.route.params.subscribe((params)=>{
-            console.log('Params event: '+params)
-          })
-
-          this.route.fragment.subscribe((fragment) => {
-            console.log('Obtain fragment : ' + fragment)
-          })
-
-          this.route.queryParams.subscribe((params) => {
-            console.log('Obtain Params : ' + params)
-          })
-
+      console.log(this.route.snapshot.queryParams);
+      console.log(this.route.snapshot.fragment)
+      this.route.queryParams
+      .subscribe((queryParams:Params)=>{
+        this.allowEdit = queryParams['allowEdit']===1 ?true:false;
+      })
+        
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
