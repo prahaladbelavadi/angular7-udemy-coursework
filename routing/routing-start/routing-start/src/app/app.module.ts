67d6b42a0c7bfe5859d2c9f62d35c4f1a2bdc951
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-27 00:52:40 +0530

    add wildcard router

--- routing/routing-start/routing-start/src/app/app.module.ts
+++ routing/routing-start/routing-start/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { Routes, RouterModule } from "@angular/router";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes:  Routes = [
   { path: '', component: HomeComponent },
@@ -21,7 +22,9 @@ const appRoutes:  Routes = [
   { path: 'servers', component: ServersComponent, children:[
     { path: ':id', component: ServerComponent },
     { path: ':id/edit', component: EditServerComponent }
-  ] }
+  ] },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
 
 ]
 
@@ -33,7 +36,8 @@ const appRoutes:  Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
