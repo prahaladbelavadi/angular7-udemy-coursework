a4a8c5db822e5e44b319519e81602082774388ca
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-26 17:29:44 +0530

    128: working; dynamically provide routes for elements displayed by routing through a list

--- routing/routing-start/routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing/routing-start/routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,9 +13,26 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private route:ActivatedRoute) { }
 
   ngOnInit() {
+    // snapshot
+          console.log(this.route.snapshot.queryParams);
+          console.log(this.route.snapshot.fragment)
+    
+    //Observable method dynamic 
+          this.route.params.subscribe((params)=>{
+            console.log('Params event: '+params)
+          })
+
+          this.route.fragment.subscribe((fragment) => {
+            console.log('Obtain fragment : ' + fragment)
+          })
+
+          this.route.queryParams.subscribe((params) => {
+            console.log('Obtain Params : ' + params)
+          })
+
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
