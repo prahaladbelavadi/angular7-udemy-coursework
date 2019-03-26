2f23ca8b28703ea10aaf092e6d5c3c28e5a4a30a
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-26 14:32:26 +0530

    119

--- routing/routing-start/routing-start/src/app/app.module.ts
+++ routing/routing-start/routing-start/src/app/app.module.ts
@@ -11,6 +11,14 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { Routes, RouterModule } from "@angular/router";
+
+const appRoutes:  Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'servers', component: ServersComponent },
+  
+]
 
 @NgModule({
   declarations: [
@@ -25,7 +33,8 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
