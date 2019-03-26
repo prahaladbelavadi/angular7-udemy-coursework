0cbe5abb8c24c972d6e607d6afc67250fefd620e
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-26 15:30:25 +0530

    dynamic data handling

--- routing/routing-start/routing-start/src/app/app.module.ts
+++ routing/routing-start/routing-start/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { Routes, RouterModule } from "@angular/router";
 const appRoutes:  Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'users/:id', component: UserComponent },
   { path: 'servers', component: ServersComponent },
   
 ]
