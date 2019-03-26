f5778253db489d75e11a019115620dd52432698f
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-27 01:13:37 +0530

    reloacting the route config to app-routing module

--- routing/routing-start/routing-start/src/app/app.module.ts
+++ routing/routing-start/routing-start/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -11,22 +12,8 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { Routes, RouterModule } from "@angular/router";
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const appRoutes:  Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent , children:[
-    { path: ':id/:name', component: UserComponent }
-  ]},
-  { path: 'servers', component: ServersComponent, children:[
-    { path: ':id', component: ServerComponent },
-    { path: ':id/edit', component: EditServerComponent }
-  ] },
-  { path: 'not-found', component: PageNotFoundComponent },
-  { path: '**', redirectTo: '/not-found' }
-
-]
 
 @NgModule({
   declarations: [
@@ -43,7 +30,7 @@ const appRoutes:  Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
