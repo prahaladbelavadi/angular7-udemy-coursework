51c32c4f005dd2f3b066f789c09765dbcbf3468d
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-27 16:08:41 +0530

    141: passing stattic message data to a route

--- routing/routing-start/routing-start/src/app/app-routing.module.ts
+++ routing/routing-start/routing-start/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { EditServerComponent } from "./servers/edit-server/edit-server.component
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { AuthGuard } from "./auth-guard.service";
 import { CanDeactivateGuard } from "can-deactivate-guard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
 
 
 const appRoutes: Routes = [
@@ -29,7 +30,11 @@ const appRoutes: Routes = [
                 { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
             ]
     },
-    { path: 'not-found', component: PageNotFoundComponent },
+    // { path: 'not-found', component: PageNotFoundComponent },
+    { path: 'not-found',
+        component: ErrorPageComponent, 
+        data: {message: 'Page not Found!'}
+     },
     { path: '**', redirectTo: '/not-found' }
 ]
 
--- routing/routing-start/routing-start/src/app/app.module.ts
+++ routing/routing-start/routing-start/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { AuthGuard } from './auth-guard.service';
 import { AuthService } from './auth.service';
 import { CanDeactivateGuard } from 'can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { CanDeactivateGuard } from 'can-deactivate-guard.service';
     UserComponent,
     EditServerComponent,
     ServerComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
