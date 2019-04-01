972a0b08a8afc55fbb837b26f1266cec3ac27d26
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-01 17:08:42 +0530

    152: cofiguring route params

--- projects/prj-directives-final/prj-directives-final/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ projects/prj-directives-final/prj-directives-final/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -11,13 +10,7 @@ import { RecipeService } from '../../recipe.service';
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor(private recipeSerivce:RecipeService) { }
-
   ngOnInit() {
-  }
-
-  onSelected() {
-    this.recipeSerivce.recipeSelected.emit(this.recipe)
   }
 
 }
