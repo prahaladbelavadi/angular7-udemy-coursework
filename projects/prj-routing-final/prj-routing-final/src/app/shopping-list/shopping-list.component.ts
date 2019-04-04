fbb8101a5ae90bc4acf63cc79f187ea672a028a7
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-04-04 15:18:24 +0530

    171: finishing up with observables

---
+++ projects/prj-routing-final/prj-routing-final/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {Subscrption} from 'rxjs/Subscrption';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit, onDestroy {
+  ingredients: Ingredient[];
+  private subscription: Subscrption;
+
+  constructor(private slService: ShoppingListService) { }
+
+  ngOnInit() {
+    this.ingredients = this.slService.getIngredients();
+    this.subscription = this.slService.ingredientsChanged
+      .subscribe(
+        (ingredients: Ingredient[]) => {
+          this.ingredients = ingredients;
+        }
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+}
