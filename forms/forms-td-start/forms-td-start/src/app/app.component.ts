d6295689ddaca53485ffdaae3166f5ebb4e5fb7a
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-05-13 11:04:52 +0530

    correct mistakes;

--- forms/forms-td-start/forms-td-start/src/app/app.component.ts
+++ forms/forms-td-start/forms-td-start/src/app/app.component.ts
@@ -1,56 +1,55 @@
-import { Component, ViewChild } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild("f") signupForm: NgForm;
-
-  defaultQuestion = "pet";
-  answer = "";
-  genders = ["male", "female"];
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'teacher';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
   submitted = false;
 
-  user = {
-    username: "",
-    email: "",
-    secretQuestion: "",
-    answer: "",
-    gender: ""
-  };
-
   suggestUserName() {
-    const suggestedName = "Superuser";
+    const suggestedName = 'Superuser';
     // this.signupForm.setValue({
     //   userData: {
     //     username: suggestedName,
     //     email: ''
-    //   }, secret: 'pet',
-    //   questionAnswer:'',
-    //   gender: 'male '
-    // })
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
     this.signupForm.form.patchValue({
-      userData: suggestedName
+      userData: {
+        username: suggestedName
+      }
     });
   }
 
-  // onSubmit(form: NgForm){
-  //   console.log(form)
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
   // }
 
   onSubmit() {
     this.submitted = true;
     this.user.username = this.signupForm.value.userData.username;
     this.user.email = this.signupForm.value.userData.email;
-    this.user.secretQuestion = this.signupForm.value.userData.secretQuestion;
-    this.user.answer = this.signupForm.value.userData.questionAnswer;
-    this.user.gender = this.signupForm.value.userData.gender;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
 
-    console.log(this.signupForm);
-  
-  this.signupForm.reset()
-
+    this.signupForm.reset();
   }
 }
