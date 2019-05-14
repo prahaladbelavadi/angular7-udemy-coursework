d962b348f50763bfad10809b3b6a157a68572d94
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-05-14 15:17:51 +0530

    finish template driven assignment

--- assignments/forms-td-assignment-start/forms-td-assignment-start/src/app/app.component.ts
+++ assignments/forms-td-assignment-start/forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
 
+  selectedSubscription = 'Advanced'
+  
+  @ViewChild('signupForm') signForm:NgForm
+  
+  onSubmit(){
+    console.log(this.signForm.value);
+  }
 }
