a3114712ee85dd35dfd22d2f9f2d01a4fb513219
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-05-15 15:02:21 +0530

    203; form control  arrays

--- forms/forms-reactive-start/forms-reactive-start/src/app/app.component.ts
+++ forms/forms-reactive-start/forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -23,9 +23,14 @@ export class AppComponent {
         'username': new FormControl(null, Validators.required),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
-      'gender': new FormControl('male')
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([]])
     })
   }
 
+  onAddHobby(){
+    const control = new FormControl(null, Validators.required);
+  (<FormArray>this.signupForm.get('hobbies')).push(control)
+  }
 
 }
