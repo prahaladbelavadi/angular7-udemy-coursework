77b1beb83b3efed3499cfa687b721f78457f3dad
Prahalad Belavadi <prahalad@belavadi.com>
2019-05-26 02:03:26 +0530

    216: to update existing form; it takes in value on select, decides which if its on edit mode and accordingly updates or adds a new item to array;

--- projects/prj-observables-final/prj-observables-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ projects/prj-observables-final/prj-observables-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -19,12 +19,12 @@ import { ShoppingListService } from '../shopping-list.service';
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
 
-  @ViewChild('f') slForm:NgForm;
+  @ViewChild('f') slForm: NgForm;
 
   subscription: Subscription;
   editMode = false;
   editedItemIndex: number;
-  editedItem:Ingredient;
+  editedItem: Ingredient;
 
   constructor(private slService: ShoppingListService) { }
 
@@ -38,7 +38,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
         this.slForm.setValue({
           name: this.editedItem.name,
           amount: this.editedItem.amount
-        })
+        });
 
       }
     );
@@ -47,11 +47,15 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   onAddItem(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.slService.addIngredient(newIngredient);
+    if (this.editMode) {
+        this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else {
+      this.slService.addIngredient(newIngredient);
+    }
   }
 
-  ngOnDestroy(){
-    this.subscription.unsubscribe()
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
