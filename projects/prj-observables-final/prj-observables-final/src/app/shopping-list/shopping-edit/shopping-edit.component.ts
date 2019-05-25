b6813190704537ee1bca699bfd145f44196f118c
Prahalad Belavadi <prahalad@belavadi.com>
2019-05-26 01:39:36 +0530

    214: allowing selection of items from the list

--- projects/prj-observables-final/prj-observables-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ projects/prj-observables-final/prj-observables-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,28 +1,47 @@
 import {
   Component,
   OnInit,
+  OnDestroy,
 } from '@angular/core';
+
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
-import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
+    this.slService.startedEditing
+    .subscribe(
+      (index: number) => {
+        this.editedItemIndex = index;
+        this.editMode = true;
+      }
+    );
   }
 
-  onAddItem(form:NgForm) {
+  onAddItem(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     this.slService.addIngredient(newIngredient);
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe()
   }
 
 }
--- projects/prj-observables-final/prj-observables-final/src/app/shopping-list/shopping-list.service.ts
+++ projects/prj-observables-final/prj-observables-final/src/app/shopping-list/shopping-list.service.ts
@@ -3,6 +3,9 @@ import { Subject } from 'rxjs/Subject';
 
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
+
+  startedEditing =  new Subject<number>();
+
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
