e728efeb84795ba4f76ea290753b1a361e13fd5a
Prahalad Belavadi <prahalad@belavadi.com>
2019-05-26 01:52:05 +0530

    215: load form content onto input to edit

--- projects/prj-observables-final/prj-observables-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ projects/prj-observables-final/prj-observables-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -2,6 +2,7 @@ import {
   Component,
   OnInit,
   OnDestroy,
+  ViewChild,
 } from '@angular/core';
 
 import { NgForm } from '@angular/forms';
@@ -18,9 +19,12 @@ import { ShoppingListService } from '../shopping-list.service';
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
 
+  @ViewChild('f') slForm:NgForm;
+
   subscription: Subscription;
   editMode = false;
   editedItemIndex: number;
+  editedItem:Ingredient;
 
   constructor(private slService: ShoppingListService) { }
 
@@ -30,6 +34,12 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
       (index: number) => {
         this.editedItemIndex = index;
         this.editMode = true;
+        this.editedItem = this.slService.getIngredient(index);
+        this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        })
+
       }
     );
   }
