3071be0efcbe111c69406babadaa64871bff7e7a
Prahalad Belavadi <prahalad@belavadi.com>
2019-05-31 00:34:34 +0530

    221: making clear button work;

--- projects/prj-observables-final/prj-observables-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ projects/prj-observables-final/prj-observables-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -57,6 +57,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     form.reset()
   }
 
+  onClear(){
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
