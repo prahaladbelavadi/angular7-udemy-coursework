0da11de4a8f1a8784125e37d508eade494e77b89
Prahalad Belavadi <prahalad@belavadi.com>
2019-05-31 00:44:19 +0530

    222: deleting items on shopping list

--- projects/prj-observables-final/prj-observables-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ projects/prj-observables-final/prj-observables-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -62,6 +62,13 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.editMode = false;
   }
 
+  onDelete(){
+    this.slService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
+
+  }
+
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
--- projects/prj-observables-final/prj-observables-final/src/app/shopping-list/shopping-list.service.ts
+++ projects/prj-observables-final/prj-observables-final/src/app/shopping-list/shopping-list.service.ts
@@ -4,7 +4,7 @@ import { Subject } from 'rxjs/Subject';
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
 
-  startedEditing =  new Subject<number>();
+  startedEditing = new Subject<number>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -15,7 +15,7 @@ export class ShoppingListService {
     return this.ingredients.slice();
   }
 
-  getIngredient(index: number){
+  getIngredient(index: number) {
     return this.ingredients[index];
   }
 
@@ -32,8 +32,13 @@ export class ShoppingListService {
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 
-  updateIngredient(index: number, newIngredient: Ingredient){
+  updateIngredient(index: number, newIngredient: Ingredient) {
     this.ingredients[index] = newIngredient;
     this.ingredientsChanged.next(this.ingredients.slice())
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
