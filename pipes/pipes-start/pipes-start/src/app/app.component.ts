5b2b0992f3d289bf059548c18e317d7f0734eaaa
Prahalad Belavadi <prahalad@belavadi.com>
2019-06-07 01:14:41 +0530

    245: pure pipe; this updates the pipe as the inputs or anything on the page changes;
    Signed-off-by: Prahalad Belavadi <prahalad@belavadi.com>

--- pipes/pipes-start/pipes-start/src/app/app.component.ts
+++ pipes/pipes-start/pipes-start/src/app/app.component.ts
@@ -35,11 +35,20 @@ export class AppComponent {
 
   filteredStatus = '';
 
-  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+  getStatusClasses(server: { instanceType: string, name: string, status: string, started: Date }) {
     return {
       'list-group-item-success': server.status === 'stable',
       'list-group-item-warning': server.status === 'offline',
       'list-group-item-danger': server.status === 'critical'
     };
+  }
+
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New service',
+      status: 'stable',
+      started: new Date(6, 7, 2019)
+    });
   }
 }
