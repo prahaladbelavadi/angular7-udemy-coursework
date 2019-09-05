6f5bd5f63f618af962945df1e5dceb1760e34358
Prahalad Belavadi <prahalad@belavadi.com>
2019-09-05 08:51:35 +0530

    adding headers and query params
    Signed-off-by: Prahalad Belavadi <prahalad@belavadi.com>

--- http-requests/http-04-loading-indicator/http-04-loading-indicator/src/app/posts.service.ts
+++ http-requests/http-04-loading-indicator/http-04-loading-indicator/src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Post } from './post.model';
 import { map, catchError } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
@@ -27,9 +27,20 @@ export class PostsService {
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('Custom', 'key');
+
     return this.http
       .get<{ [key: string]: Post }>(
-        'https://ng-complete-guide-udemy.firebaseio.com/posts.json'
+        'https://ng-complete-guide-udemy.firebaseio.com/posts.json', {
+          headers: new HttpHeaders({
+            'Custom-Header': 'Hello'
+          }),
+          params: searchParams
+          // params: new HttpParams().set('print', 'pretty')
+        }
       )
       .pipe(
         map(responseData => {
